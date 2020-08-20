93a318da87cc2d4684e9cce7635153253a1ef5aa
unknown <nitinmakin124.com>
2020-08-20 22:42:25 +0530

    Added-UC2 get the random number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,7 +1,14 @@
+import java.util.Random;
 public class SnakeLadder
    {
      public static void StartFrom0()
     {
      System.out.println("Player Start from 0");
+    }
+    public static void GetRandomNo1To6()
+    {
+        Random random = new Random();
+	int RandonNo1To6 = random.nextInt(6)+1;
+	System.out.println("random number is "+RandonNo1To6);
     }
    }
