fe2d4743d4b32a398cd342d58e248c8b92b02c3e
unknown <nitinmakin124.com>
2020-08-21 00:54:06 +0530

    Added- UC4 repete till the player reach 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -9,33 +9,61 @@ public class SnakeLadder
 	{
 		Random random = new Random();
 		int RandonNo1To6 = random.nextInt(6)+1;
-		System.out.println("random number is "+RandonNo1To6);
+		//System.out.println("random number is "+RandonNo1To6);
 		return RandonNo1To6;
 	}
 
-         
-	public static void CheckSnake_Ladder_Noplay()
+
+	public static void RepeteTillPlayerReach100()
 	{ 
-         int RandonNo1To6=GetRandomNo1To6();
-         Random random = new Random();
-         int RandomCheck=random.nextInt(3);
-         	if(RandomCheck == 2)
+
+		Random random = new Random();
+		int Win=0;
+		while(Win < 100)
+		{
+			int RandonNo1To6=GetRandomNo1To6();
+			int RandomCheck=random.nextInt(3);
+			if(RandomCheck == 2 && Win > RandonNo1To6)
 			{
-				System.out.println("oops you step on snake you will got 2 step back");
+
+				System.out.println("oops you step on snake you will go "+RandonNo1To6+" step back");
+				Win=Win-RandonNo1To6;
+				System.out.println("you are in "+Win);
+
 			}
 			else if(RandomCheck == 0)
 			{
-				System.out.println("you made a foult so you will be in same position");
+				System.out.println("you made a foult you scored 0 so you will be in same position");
+				System.out.println("you are in "+Win);
+
 			}
-			else
-				System.out.println("congrats you have incremented by "+RandonNo1To6);
+			else 
+			{
+				Win=Win+RandonNo1To6;
+				if(Win > 100)
+				{
+					Win=Win-RandonNo1To6;
+				}
+				else {
+					System.out.println("congrats you have incremented by "+RandonNo1To6);
+					System.out.println("you are in "+Win);
+				}
+			}
+			if(Win == 100)
+			{
+				System.out.println("congrats you have reached till 100");
 
-        }
+			}
+		}
 
+
+	}
 	public static void main(String[] args)
 	{
 		StartFrom0();
-		System.out.println(GetRandomNo1To6());
-		CheckSnake_Ladder_Noplay();
+		System.out.println("..............");
+
+		RepeteTillPlayerReach100();
 	}
- }
+}
+
