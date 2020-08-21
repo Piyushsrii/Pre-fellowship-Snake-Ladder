3197f6e4210ca6ce9aed5f582fc0fd1d8fa32a6d
unknown <nitinmakin124.com>
2020-08-21 17:13:51 +0530

    Refactor code- changing naming convention in UC6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,66 +1,66 @@
 import java.util.Random;
 public class SnakeLadder
 {
-	public static void StartFrom0()
+	public static void startFrom0()
 	{
 		System.out.println("You are Starting from 0");
 	}
-	public static int GetRandomNo1To6()
+	public static int getRandomNo1To6()
 	{
 		Random random = new Random();
-		int RandonNo1To6 = random.nextInt(6)+1;
+		int randonNo1To6 = random.nextInt(6)+1;
 		//System.out.println("random number is "+RandonNo1To6);
-		return RandonNo1To6;
+		return randonNo1To6;
 	}
 
 
-	public static int GetNoOfTimesDicePlayed_Position()
+	public static int getNoOfTimesDicePlayed_Position()
 	{ 
 
 		Random random = new Random();
-		int NoOfTimesDicePlayed=0;
-		int Win=0;
+		int noOfTimesDicePlayed=0;
+		int Win = 0;
 		while(Win < 100)
 		{
-			int RandonNo1To6=GetRandomNo1To6();
-			int RandomCheck=random.nextInt(3);
+			int randonNo1To6 = getRandomNo1To6();
+			int randomCheck = random.nextInt(3);
 			//if player position will go below 0 then player will start from 0
 
 			//when step on snake
-			if(RandomCheck == 2 && Win > RandonNo1To6)
+			if(randomCheck == 2 && Win > randonNo1To6)
 			{
 
-				System.out.println("OOPS.. you step on snake you will go "+RandonNo1To6+" step back");
-				Win=Win-RandonNo1To6;
+				System.out.println("OOPS.. you step on snake you will go "+randonNo1To6+" step back");
+				Win = Win-randonNo1To6;
 				System.out.println("You are in position "+Win);
 				System.out.println();
-				NoOfTimesDicePlayed++;
+				noOfTimesDicePlayed++;
 			}
 			// no move 
-			else if(RandomCheck == 0)
+			else if(randomCheck == 0)
 			{
 				System.out.println("You made a FOUL you scored 0 so you will be in same position");
 				System.out.println("You are in position "+Win);
 				System.out.println();
-				NoOfTimesDicePlayed++;
+				noOfTimesDicePlayed++;
 
 			}
 
 			else 
 			{
-				Win=Win+RandonNo1To6;
+				Win = Win+randonNo1To6;
 				//if win exceed 100
 				if(Win > 100)
 				{
-					Win=Win-RandonNo1To6;
-					NoOfTimesDicePlayed++;
+					Win = Win-randonNo1To6;
+					noOfTimesDicePlayed++;
 				}
 				else {
-					System.out.println("You have scored "+RandonNo1To6);
-					System.out.println("CONGRATS... you have incremented by "+RandonNo1To6);
+					System.out.println("You have scored "+randonNo1To6);
+					System.out.println("CONGRATS... you have incremented by "+randonNo1To6);
 					System.out.println("You are in position "+Win);
 					System.out.println();
-					NoOfTimesDicePlayed++;
+					noOfTimesDicePlayed++;
 				}
 			}
 			if(Win == 100)
@@ -70,14 +70,14 @@ public class SnakeLadder
 
 			}
 		}
-		return NoOfTimesDicePlayed;
+		return noOfTimesDicePlayed;
 
 	}
 	public static void main(String[] args)
 	{
-		StartFrom0();
+		startFrom0();
 		System.out.println("..............");
-		System.out.println("Total times dice played by you is "+GetNoOfTimesDicePlayed_Position());
+		System.out.println("Total times dice played by you is "+getNoOfTimesDicePlayed_Position());
 		System.out.println();
 		System.out.println("Thankyou for playing game...");
 	}
